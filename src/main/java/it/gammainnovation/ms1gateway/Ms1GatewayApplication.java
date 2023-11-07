f15abb63acda6f25ed2dc4ccb6cee16b23fa1308
frabarbara <francesco.barbara5697@gmail.com>
2023-11-07 02:47:43 +0100

    initial commit

---
+++ src/main/java/it/gammainnovation/ms1gateway/Ms1GatewayApplication.java
@@ -0,0 +1,13 @@
+package it.gammainnovation.ms1gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms1GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ms1GatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/gammainnovation/ms1gateway/Ms1GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gammainnovation.ms1gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms1GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
