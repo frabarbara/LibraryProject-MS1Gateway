9d69c4bf50b0e3a19231bb7c0f559d6e5fc8eae9
frabarbara <francesco.barbara5697@gmail.com>
2023-11-13 11:59:12 +0100

    create MS1 controller

---
+++ src/main/java/it/gammainnovation/ms1gateway/Controller.java
@@ -0,0 +1,32 @@
+package it.gammainnovation.ms1gateway;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+public class Controller {
+
+    @PostMapping("/signup")
+    public void signUp(@RequestBody User newUser) {     //Angular service will send JSON object
+
+    }
+
+    @PostMapping("/login")
+    public String signIn(@RequestBody Object credentials) {
+        return "ciao";
+    }
+
+    @GetMapping("/library/booksearch")
+    public ArrayList<Book> searchBook(
+            @RequestParam String title,
+            @RequestParam String author,
+            @RequestParam int edition,
+            @RequestParam String theme,
+            @RequestParam int pubYear
+    ) {
+        ArrayList<Book> foundBooks = new ArrayList<>();
+        /* [...] */
+        return foundBooks;
+    }
+}
