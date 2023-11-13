02c1fdd1de447ecb38bcd8f25290a78cebf89a38
frabarbara <francesco.barbara5697@gmail.com>
2023-11-13 16:26:34 +0100

    start implementation signup API

--- src/main/java/it/gammainnovation/ms1gateway/Controller.java
+++ src/main/java/it/gammainnovation/ms1gateway/Controller.java
@@ -3,15 +3,25 @@ package it.gammainnovation.ms1gateway;
 import it.gammainnovation.librarymodel.User;
 import it.gammainnovation.librarymodel.Book;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 @RestController
 public class Controller {
 
     @PostMapping("/signup")
-    public void signUp(@RequestBody User newUser) {     //Angular service will send JSON object
+    public User signUp(@RequestBody User newUser) {     //Angular service will send JSON object
 
+        RestTemplate myRestTemplate = new RestTemplate();
+        User signedUpUser = myRestTemplate.postForObject(
+                "http://localhost:8083/signup",
+                newUser,
+                User.class
+        );
+
+        return signedUpUser;
     }
 
     @PostMapping("/login")
